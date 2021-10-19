6be988d0ced28f10cc22c2e4d2de9db3998c537c
Guilherme Biage <gbiage@pop-os.localdomain>
2021-10-19 15:19:32 -0300

    Changed db connection info

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,9 +3,9 @@ const mysql = require('mysql')
 const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: '',
-    database: 'agenda-petshop'
+    user: 'mysql',
+    password: 'admin',
+    database: 'pet_api'
 })
 
 module.exports = conexao
